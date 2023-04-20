ae0e1bb9748509e20205b5e2ac766a7f06d0e196
mrai1127 <mausamrai11@gmail.com>
2023-04-19 23:27:00 -0400

    Revert "unnecessary commit"
    This reverts commit 671ddf52bd82e17615190d66543178225a51ffd5.

--- src/com/messaging/queue/MessageQueueExample.java
+++ src/com/messaging/queue/MessageQueueExample.java
@@ -1,5 +1,50 @@
 package com.messaging.queue;
 
+import java.sql.Connection;
+
 public class MessageQueueExample {
+
+	private static final String BROKER_URL = "tcp://localhost:61616";
+	private static final String QUEUE_NAME = "test_Queue";
+
+	public static void main(String[] args) {
+
+		// create a connection to the message broker
+		ConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		// crate a session for sending and receicing messages
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWKEDGE);
+
+		// create a queue to send and receive messages from
+		Destination queue = session.createQueue(QUEUE_NAME);
+
+		// create a producer to send messages to the queue
+		MessageProducer producer = session.createProducer(queue);
+
+		// send a message to the queue
+		String messageText = "Hello, world!";
+		TextMessage message = session.createTextMessage(messageText);
+		producer.send(message);
+		System.out.println("Sent message: " + messageText);
+
+		// create a consumer to recieve messages form the queue
+		MessageConsumer consumer = session.createConsumer(queue);
+
+		// wait for a message to be received
+		Message recievedMessage = consumer.recieve();
+		if (recievedMessage instanceof TextMessage) {
+			String recievedMessageText = ((TextMessage) recievedMessage).getText();
+			System.out.println("Received message: " + receivedMessageText);
+		}
+
+		// clean up resources
+		consumer.close();
+		producer.close();
+		session.close();
+		connection.close();
+
+	}
 
 }
--- src/com/rai/algorithm/UnNecessary.java
+++
@@ -1,8 +0,0 @@
-package com.rai.algorithm;
-
-public class UnNecessary {
-	public static void main(String[] args) {
-		System.out.println("Just checking for reverting");
-	}
-
-}
